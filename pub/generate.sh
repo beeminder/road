3dbfa91257e07beaaaddf30d74077971476b8f71
Uluc Saranli <saranli@gmail.com>
2018-03-16 22:58:01 +0300

    - Added waiting for chrome to finish

--- pub/generate.sh
+++ pub/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-google-chrome --allow-file-access-from-files --disable-web-security --headless --deterministic-fetch --disable-gpu --no-sandbox --dump-dom file:///home/saranli/Research/Projects/road/pub/generate.html > output.svg
+google-chrome --virtual-time-budget=60000 --allow-file-access-from-files --disable-web-security --headless --deterministic-fetch --disable-gpu --no-sandbox --dump-dom file:///home/saranli/Research/Projects/road/pub/generate.html > output.svg
 
 google-chrome --allow-file-access-from-files --disable-web-security --headless --deterministic-fetch --disable-gpu --no-sandbox --window-size=710,460 --screenshot file:///home/saranli/Research/Projects/road/pub/output.svg
