f44053cdb4ae4277be63f253b9377fb8c743c69b
Uluc Saranli <saranli@gmail.com>
2018-03-25 00:54:19 +0300

    - Cleaned up and tested rendering

--- jsbrain_server/renderer.js
+++ jsbrain_server/renderer.js
@@ -4,72 +4,61 @@ const puppeteer = require('puppeteer')
 
 class Renderer {
   
-  constructor(browser) {
+  constructor(browser, id) {
     this.browser = browser
     this.page = null
+    this.id = id;
   }
 
-  async createPage(url, { timeout, waitUntil }) {
+  async loadPage( url ) {
     let gotoOptions = {
-      timeout: Number(timeout) || 30 * 1000,
-      waitUntil: waitUntil || 'networkidle2'
+      timeout: 30 * 1000,
+      waitUntil: 'networkidle2'
     }
 
-
+    // Create page if one does not exist and install console log handler
     if (this.page == null) {
       var curPages = await this.browser.pages();
       if (curPages.length == 0)
         this.page = await this.browser.newPage()
       else 
         this.page = curPages[0]
-      this.page.on('console', msg => console.log('PAGE LOG:', msg.text()));    
+      this.page.on('console', 
+                   msg => console.log("("+this.id+"): PAGE LOG:", 
+                                      msg.text()));    
     }
     const page = this.page;
 
+    // Render the page and return result
     await page.goto(url, gotoOptions)
     return page
   }
 
-  async render(url, options) {
-    let page = null
-    const { timeout, waitUntil } = options
+  async render(url) {
+    var slug = url.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, '')
 
-    console.time('rendering')
-    page = await this.createPage(url, { timeout, waitUntil })
+    // Load and render the page, extract html
+    console.time('rendering'+this.id)
+    let page = await this.loadPage(url)
     const html = await page.content()
-    console.timeEnd('rendering')
+    console.timeEnd('rendering'+this.id)
 
-    return html
-  }
+    // Take screenshot of rendered page
+    console.time('screenshot'+this.id)
+    const buffer 
+            = await page.screenshot({path:slug+".png", 
+                                     clip:{x:0, y:0, width:710, height:460}})
+    console.timeEnd('screenshot'+this.id)
 
-  async screenshot(url, options) {
-    let page = null
-    try {
-      const { timeout, waitUntil, ...extraOptions } = options
-      page = await this.createPage(url, { timeout, waitUntil })
-
-      const { fullPage, omitBackground } = extraOptions
-      const buffer = await page.screenshot({
-        ...extraOptions,
-        fullPage: fullPage === 'true',
-        omitBackground: omitBackground === 'true',
-      })
-      return buffer
-    } finally {
-      if (page) {
-        await page.close()
-      }
-    }
-  }
-
-  async close() {
-    await this.browser.close()
+    return {html: html, png: buffer}
   }
 }
 
-async function create() {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--allow-file-access-from-files'] })
-  return new Renderer(browser)
+async function create( id ) {
+  const browser 
+          = await puppeteer.launch({ args: ['--no-sandbox', 
+                                            '--allow-file-access-from-files'] })
+  return new Renderer(browser, id)
 }
 
 module.exports = create
