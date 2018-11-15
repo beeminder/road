35105415ab684f19b33234d8a73570e4f42048bd
Uluc Saranli <saranli@gmail.com>
2018-11-15 11:53:13 +0300

    Cleaned up jsbrain server and manual instances and updated README

--- jsbrain_manual/generate.sh
+++ jsbrain_manual/generate.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 FILENAME" >&2
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 BASE PATH" >&2
   exit 1
 fi
 
-if ! [ -f "$1" ]; then
-  echo "Could not find file $1" >&2
+BASE=$2"/"$1
+BBFILE=$BASE".bb"
+PNGFILE=$1".png"
+SVGFILE=$1".svg"
+URL="file://"$BBFILE
+
+if ! [ -f "$BBFILE" ]; then
+  echo "Could not find file $BBFILE" >&2
   exit 1
 fi
 
-echo Processing file://${PWD}/generate.html?bb="$1"
+echo Processing file://${PWD}/generate.html?bb="$URL"
 
-chromium-browser --virtual-time-budget=60000 --allow-file-access-from-files --disable-web-security --headless --deterministic-fetch --disable-gpu --no-sandbox --dump-dom file://${PWD}/generate.html?bb="$1" > output.txt
+chromium-browser --virtual-time-budget=60000 --allow-file-access-from-files --disable-web-security --headless --deterministic-fetch --disable-gpu --no-sandbox --dump-dom file://${PWD}/generate.html?bb="$URL" > output.txt
 
-sed -n -e 's/.*<body>\(.*\)<\/body>.*/\1/p' < output.txt > output.svg
+sed -n -e 's/.*<body>\(.*\)<\/body>.*/\1/p' < output.txt > $SVGFILE
 rm output.txt
 
-chromium-browser --allow-file-access-from-files --disable-web-security --headless --deterministic-fetch --disable-gpu --no-sandbox --window-size=710,460 --screenshot file://${PWD}/output.svg
+chromium-browser --allow-file-access-from-files --disable-web-security --headless --deterministic-fetch --disable-gpu --no-sandbox --window-size=710,460 --screenshot file://${PWD}/$SVGFILE
+mv screenshot.png $PNGFILE
