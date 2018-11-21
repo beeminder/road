37199c413c30d72fb14ae575ccd876a18924efc1
Uluc Saranli <saranli@gmail.com>
2018-11-21 22:59:54 +0300

    Started sanity.js

---
+++ sanity/sanity.js
@@ -0,0 +1,8 @@
+
+const Polyfit = require('../pub/polyfit.js')
+const bu = require('../pub/butil.js')
+const br = require('../pub/broad.js')
+const bb = require('../pub/beebrain.js')
+
+
+console.log("deneme")
