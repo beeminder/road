ecc0d732adb7358d5ca650a11f1a6743460460e0
Uluc Saranli <saranli@gmail.com>
2018-11-21 18:53:25 +0300

    Fixed node server and compare.html with Promises to wait until image generation is complete.

--- jsbrain_server/renderer.js
+++ jsbrain_server/renderer.js
@@ -9,7 +9,7 @@ class Renderer {
   
   constructor(browser, id) {
     this.browser = browser
-    this.id = id;
+    this.id = id
   }
 
   // Creates a new page in a tab within the puppeteer chrome instance
@@ -23,42 +23,54 @@ class Renderer {
     // other
     const page = await this.browser.newPage()
     page.on('console', 
-            msg => console.log("("+this.id+"): PAGE LOG:", 
-                               msg.text()));    
-    page.on('error', msg => console.log("ERROR: "+msg));    
+            msg => console.log("("+this.id+"): PAGE LOG:", msg.text()))
+    page.on('error', msg => console.log("ERROR: "+msg))
 
     // Render the page and return result
     try {
       await page.goto(url, gotoOptions)
     } catch (error) {
       console.log(error)
-      await page.close();
-      return null;
+      await page.close()
+      return null
     } 
     return page
   }
 
+  /** Returns the base URL for graph and thumbnail links */
   BBURL() { return "http://brain.beeminder.com/" }
   
+  /** Checks whether a slug has disabled graphing or not */
   nograph(slug) { return slug.match(/NOGRAPH_/) }
-  // Take a file and return a parallel temp file, ie, with the directory and file 
-  // extension the same but with the base file name amended with a unique string 
-  // based on a session ID. Eg, foo.x -> foo-tmp743829.x
+
+  /** Takes a filename and return a parallel temp filename, ie, with
+      the directory and file extension the same but with the base file
+      name amended with a unique string based on a session ID. Eg,
+      foo.x -> foo-tmp743829.x */
   tempify(f,id) { return f.replace(/^(.*)\.([^\.]*)$/,"$1-tmp"+id+".$2") }
   
   /** Renders the graph associated with the BB file with the supplied
-   fbase name (i.e. user+slug), located in the supplied path in the puppeteer
-   instance. Creates a new tab, renders the graph and json, outputs
-   the graph in PNG and SVG forms as well as the JSON output and
-   closes the tab afterwards */
+      fbase name (i.e. user+slug), located in the supplied path in the puppeteer
+      instance. Creates a new tab, renders the graph and json, outputs
+      the graph in PNG and SVG forms as well as the JSON output and
+      closes the tab afterwards */
   async render(inpath, outpath, slug) {
     let page = null
-    const bburl = encodeURIComponent(inpath)+"/"+encodeURIComponent(slug)+".bb"
+
+    if (!fs.existsSync(outpath)) {
+      let err = `Could not find directory ${outpath}`
+      process.stdout.write("ERROR: "+err+"\n")
+      return { error:err }
+    }
 
     const bbfile = `${inpath}/${slug}.bb`
     if (!fs.existsSync(bbfile)) {
-      return {}
+        let err = `Could not find file ${bbfile}`
+        process.stdout.write("ERROR: "+err+"\n")
+        return { error: err}
     }
+    
+    const bburl = encodeURIComponent(inpath)+"/"+encodeURIComponent(slug)+".bb"
 
     const newid = uuidv4();
     const base = inpath // Pybrain compatibility
@@ -66,8 +78,7 @@ class Renderer {
     
     process.stdout.write(sluga+" @ ")
     let starttm = new Date()
-    process.stdout.write(starttm.toISOString().replace(/T/, ' ').replace(/\..+/, ''))
-    process.stdout.write("\n")
+    process.stdout.write(starttm.toISOString().replace(/T/,' ').replace(/\..+/,'')+"\n")
 
     const imgf = `${outpath}/${this.nograph(slug)?"NOGRAPH":slug}.png`
     const svgf = `${outpath}/${this.nograph(slug)?"NOGRAPH":slug}.svg`
@@ -78,6 +89,7 @@ class Renderer {
       var imgftmp = this.tempify(imgf, newid)
       var svgftmp = this.tempify(svgf, newid)
       var thmftmp = this.tempify(thmf, newid)
+      // Remove any existing graph and thumbnail files
       if (fs.existsSync(imgf)) fs.unlinkSync( imgf )
       if (fs.existsSync(svgf)) fs.unlinkSync( svgf )
       if (fs.existsSync(thmf)) fs.unlinkSync( thmf )
@@ -91,92 +103,108 @@ class Renderer {
       // Load and render the page, extract html
       var time_id = `Render time (${slug}, ${newid})`
       console.time(time_id)
-      if (!fs.existsSync(bbfile)) {
-        let err = `Could not find file ${bbfile}`
-        process.stdout.write("ERROR: "+err+"\n")
-        return { error: err}
-      }
-      if (!fs.existsSync(outpath)) {
-        let err = `Could not find directory ${outpath}`
-        process.stdout.write("ERROR: "+err+"\n")
-        return { error:err }
-      }
+
       page = await this.createPage(url)
+
       if (page) {
         html = await page.content()
         console.timeEnd(time_id)
       
-        // Extract and write the goal JSON
-        let jf = `${outpath}/${slug}.json`
-        let jtmp = this.tempify(jf, newid)
-        if (fs.existsSync(jf)) fs.renameSync(jf, jtmp )
+        // Extract goal stats from the JSON field and extend with file locations
         const jsonHandle = await page.$('#goaljson');
         jsonstr = await page.evaluate(json => json.innerHTML, jsonHandle);
         json = JSON.parse(jsonstr)
         json.graphurl=this.BBURL()+imgf
         json.svgurl=this.BBURL()+svgf
         json.thumburl=this.BBURL()+thmf
-        fs.writeFileSync(jtmp, JSON.stringify(json));   
-        fs.renameSync(jtmp, jf )
+
+        // Write to the goal JSON, using an intermediate temp file
+        let jf = `${outpath}/${slug}.json`
+        let jtmp = this.tempify(jf, newid)
+        if (fs.existsSync(jf)) fs.renameSync(jf, jtmp )
+        fs.writeFileSync(jtmp, JSON.stringify(json));  
+        if (fs.existsSync(jtmp)) fs.renameSync(jtmp, jf )
+        // Display statsum on node console
         process.stdout.write(json.statsum.replace(/\\n/g, '\n'))
         
-        const svgHandle = await page.$('svg')
-        svg = await page.evaluate(svg => svg.outerHTML, svgHandle);
-        svg = '<?xml version="1.0" standalone="no"?>\n'+svg
-        // write the SVG file
-        fs.writeFile(`${outpath}/${slug}.svg`, svg, (err) => {  
-          if (err) console.log(`Error saving to ${slug}.svg`);
-        });   
+        if (graphit) {
+          // Extract and write the SVG file
+          const svgHandle = await page.$('svg')
+          svg = await page.evaluate(svg => svg.outerHTML, svgHandle)
+          svg = '<?xml version="1.0" standalone="no"?>\n'+svg
+          // write to the temp SVG file and rename
+          fs.writeFileSync(svgftmp, svg);  
+          if (fs.existsSync(svgftmp)) fs.renameSync(svgftmp, svgf )
 
-        // Extract the bounding box for the zoom area to generate the thumbnail
-        var za = await page.$('.zoomarea')
-        var zi = await page.evaluate(()=>{
-          var z = document.getElementsByClassName('zoomarea')[0]
-          var b = z.getBBox()
-          var c = z.getAttribute('color')
-          return {x:Math.round(b.x+1), y:Math.round(b.y),
-                  width:Math.round(b.width-2), height:Math.round(b.height),
-                  color:c};})
-        //console.info("Zoom area bounding box is "+JSON.stringify(zi))
+          // Extract the bounding box for the zoom area to generate
+          // the thumbnail cropping boundaries
+          var za = await page.$('.zoomarea')
+          var zi = await page.evaluate(()=>{
+            var z = document.getElementsByClassName('zoomarea')[0]
+            var b = z.getBBox()
+            return {x:Math.round(b.x+1), y:Math.round(b.y),
+                    width:Math.round(b.width-2), height:Math.round(b.height)};})
+          //console.info("Zoom area bounding box is "+JSON.stringify(zi))
         
-        time_id = `Screenshot time (${slug}, ${newid})`
+          // Take a screenshot to generate PNG files
+          time_id = `Screenshot time (${slug}, ${newid})`
+          console.time(time_id)
 
-        // Take screenshot of rendered page
-        console.time(time_id)
-        const rect = await page.evaluate(selector => {
-          const element = document.querySelector(selector);
-          const {x, y, width, height} = element.getBoundingClientRect();
-          return {left: x, top: y, width, height, id: element.id};
-        }, "svg");  
-        var pngfile = `${outpath}/${slug}.png`
-        var thumbfile = `${outpath}/${slug}-thumb.png`
-        png = await page.screenshot({path:pngfile, 
-                                     clip:{x:rect.left, y:rect.top, 
-                                           width:rect.width, height:rect.height}})
-        // Generate thumbnail
-        var thmratio = 140/zi.height
-        var thmw = Math.round(zi.width*thmratio)-4
-        var thmh = Math.round(zi.height*thmratio)-4
-        thmw = 212-4; thmh = 140-4
-        gm(pngfile)
-          .in('-crop').in(zi.width+"x"+zi.height+"+"+zi.x+"+"+zi.y)
-          .in('-resize').in(thmw+'x'+thmh)
-          .in('-bordercolor').in(zi.color)
-          .in('-border').in('2x2')
-          .in('-filter').in('Box')
-          .in('-filter').in('Box')
-          .in('-remap').in('palette.png')
-          .in('-colors').in('256')
-          .in('+dither')
-          .in('+repage')
-          .write(thumbfile, (err)=>{if (err) console.log(err)})
-        // Perform palette remap using ImageMagick
-        gm(pngfile)
-          .in('-filter').in('Box')
-          .in('-remap').in('palette.png')
-          .in('-colors').in('256')
-          .in('+dither')
-          .write(pngfile, (err)=>{if (err) console.log(err)})
+          // Extract SVG boundaries on page
+          const rect = await page.evaluate(s => {
+            const element = document.querySelector(s);
+            const {x, y, width, height} = element.getBoundingClientRect();
+            return {left: x, top: y, width, height, id: element.id};
+          }, "svg");  
+          png = await page.screenshot({path:imgftmp, 
+                                       clip:{x:rect.left, y:rect.top, 
+                                             width:rect.width, height:rect.height}})
+
+          // Generate palette optimized thumbnail through cropping with ImageMagick
+          //var thmratio = 140/zi.height
+          var thmw = 212-4 // = Math.round(zi.width*thmratio)-4
+          var thmh = 140-4 // = Math.round(zi.height*thmratio)-4
+          console.info("thm request begin");
+          var res
+          res = await new Promise( (resolve, reject) => {
+            gm(imgftmp)
+              .in('-crop').in(zi.width+"x"+zi.height+"+"+zi.x+"+"+zi.y)
+              .in('-resize').in(thmw+'x'+thmh)
+              .in('-bordercolor').in(json.color)
+              .in('-border').in('2x2')
+              .in('-filter').in('Box')
+              .in('-filter').in('Box')
+              .in('-remap').in('palette.png')
+              .in('-colors').in('256')
+              .in('+dither')
+              .in('+repage')
+              .write(thmftmp, (err) => {
+                if (err) reject(err)
+                console.info("thm completed");
+                resolve(null)
+              })
+          })
+          console.info("thm request end");
+          if (fs.existsSync(thmftmp)) fs.renameSync(thmftmp, thmf )
+          
+          // Generate final graph PNG by palette remapping using ImageMagick
+          console.info("img request begin");
+          res = await new Promise( (resolve, reject) => {
+            gm(imgftmp)
+              .in('-filter').in('Box')
+              .in('-remap').in('palette.png')
+              .in('-colors').in('256')
+              .in('+dither')
+              .write(imgftmp, (err) => {
+                if (err) reject(err)
+                console.info("img completed");
+                resolve(null)
+              })
+          })
+        }
+        console.info("img request end");
+        if (fs.existsSync(imgftmp)) fs.renameSync(imgftmp, imgf )
+        
         console.timeEnd(time_id)
       } else {
 
