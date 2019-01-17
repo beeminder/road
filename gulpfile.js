a03cd5bce40f712fdf39bfa6b1eee1f23258c510
Uluc Saranli <saranli@ceng.metu.edu.tr>
2019-01-18 00:21:39 +0300

    Progress on typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,92 @@
+/**
+Main GULP file for jsbrain
+
+The following main targets are implemented
+
+*/
+
 const gulp = require('gulp')
-const minify = require('gulp-minify')
-const concat = require('gulp-concat')
-const changed = require('gulp-changed')
-const cleancss = require('gulp-clean-css')
-const ts = require("gulp-typescript")
-const tsProject = ts.createProject("tsconfig.json")
+const browserify = require("browserify");
+const source = require('vinyl-source-stream');
+const tsify = require("tsify");
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
 const jsdoc = require("gulp-jsdoc3")
+const typedoc = require("gulp-typedoc")
 
-const LIBDIR = 'lib'
-const LIBJS = LIBDIR+"/js"
-const LIBCSS = LIBDIR+"/css"
-const LIBIMG = LIBDIR+"/images"
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
 const DOCDIR = './docs'
+const TSDOCDIR = './tdocs'
+const TESTSDIR = './tests'
 
-function clean_css() {
-  return gulp.src(['src/jsbrain.css','src/pikaday.css','src/editorpage.css'])
-    .pipe(changed(LIBCSS))
-    .pipe(cleancss())
-    .pipe(gulp.dest(LIBCSS))
-}
+var paths = {
+    pages: [TESTSDIR+'/ts_test.html']
+};
 
-function compress_js() {
-  return gulp.src(['src/butil.js','src/broad.js','src/beebrain.js',
-                   'src/bgraph.js',
-                   'src/bsandbox.js',
-                   'src/btest.js',
-                   'src/client.js',
-                   'src/polyfit.js',
-                   'src/pikaday.js'])
-    .pipe(changed(LIBJS))
-    .pipe(minify())
-    .pipe(gulp.dest(LIBJS))
-}
 
-function combine_js() {
-  return gulp.src(['lib/js/butil.js','lib/js/broad.js','lib/js/beebrain.js'])
-    .pipe(concat('jsbrain.js'))
-    .pipe(gulp.dest(LIBJS))
-}
-
-function combine_jsmin() {
-  return gulp.src(['lib/js/butil-min.js','lib/js/broad-min.js',
-                   'lib/js/beebrain-min.js'])
-    .pipe(concat('jsbrain-min.js'))
-    .pipe(gulp.dest(LIBJS))
-}
-
-function copy_vendor() {
-  return gulp.src(['node_modules/moment/min/moment.min.js'])
-    .pipe(gulp.dest(LIBJS))
+function copyhtml() {
+  return gulp.src(paths.pages)
+    .pipe(gulp.dest("dist"));
 }
 
 function tscompile() {
   return tsProject.src()
     .pipe(tsProject())
-    .js.pipe(gulp.dest("temp"));
+    .js.pipe(gulp.dest("dist"));
 }
 
-function gendoc() {
+function tsbundle() {
+    return browserify({ 
+      standalone: 'bb',
+      basedir: '.',
+      debug: true,
+      entries: ['src/butil.ts'],
+      cache: {},
+      packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest("dist"));
+}
+
+function doc() {
   return gulp.src(['README.md', 'src/*.js'], {read: false})
     .pipe(jsdoc({opts: {destination: DOCDIR, tutorials:"./tutorials"}}));
 }
 
-exports.compile = gulp.series(compress_js,
-                              gulp.parallel(combine_js, combine_jsmin, clean_css),
-                              copy_vendor
-                             ) 
+function tdoc() {
+  return gulp.src(['src/*.ts'])
+    .pipe(typedoc({
+            // TypeScript options (see typescript docs)
+            module: "commonjs",
+            target: "es5",
+            includeDeclarations: true,
+            excludeExternals: true,
+ 
+            // Output options (see typedoc docs)
+            out: TSDOCDIR,
+ 
+            // TypeDoc options (see typedoc docs)
+            name: "jsbrain",
+            mode: "modules",
+            includes: "./tutorials",
+            ignoreCompilerErrors: false,
+            version: true,
+    }));
+}
+
 exports.tscompile = tscompile
 
-exports.gendoc = gendoc
+exports.tsbundle = tsbundle
+
+exports.tsmake = gulp.series(copyhtml, gulp.series(tscompile, tsbundle))
+
+exports.doc = doc
+
+exports.tdoc = tdoc
