3561dc350474596192b8556fb3912d257c32a1c5
Daniel Reeves <dreeves@beeminder.com>
2019-08-12 21:18:04 -0700

    Make tluz say today if red yesterday #offred

--- src/broad.js
+++ src/broad.js
@@ -237,14 +237,13 @@
   /** Returns the number of days to derail for the current road
       TODO: There are some issues with computing tcur, vcur */
   self.dtd = ( rd, goal, t, v ) => {
+    if (goal.offred && self.isLoser(rd, goal, null, t, v)) {
+      return 0 // override if offred & red yesterday (cuz do-less loophole)
+    }
+
     var tnow = goal.tcur
     var fnw = (self.gdelt(rd, goal, t,v) >= 0)?0.0:goal.nw // future noisy width
     var elnf = (x) => (Math.max(goal.lnf(x),fnw)) //eff. lane width
-
-    if (goal.offred) {
-      // override and return 0 if offred & red yesterday (cuz do less loophole)
-        
-    }
 
     var x = 0 // the number of steps  
     var vpess = v // the value as we walk forward w/ pessimistic presumptive reports
