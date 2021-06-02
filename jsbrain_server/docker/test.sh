be8b68f4799bde4d9c0230d68d2695d6425a93a1
Adam Wolf <adamwolf@feelslikeburning.com>
2021-06-02 10:50:58 -0500

    .

--- jsbrain_server/docker/test.sh
+++ jsbrain_server/docker/test.sh
@@ -11,7 +11,7 @@ echo "Waiting for jsbrain to be ready..."
 counter=0
 while ! nc -z jsbrain 8777; do
 	sleep 1;
-	if [ counter -gt 10 ]; then
+	if [ $counter -gt 10 ]; then
 		echo "Timeout looking for jsbrain. Exiting."
 		exit 1
 	fi
