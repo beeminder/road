e82b3e3186fac709eb2e42c1382d7656d0d90cba
Daniel Reeves <dreeves@beeminder.com>
2025-07-10 18:09:09 -0700

    Move favicons

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
 const gulp = require('gulp')
 const minify = require('gulp-minify')
 const concat = require('gulp-concat')
-//const changed = require('gulp-changed') #SCHDEL
 const changed = require('gulp-changed').default
 const cleancss = require('gulp-clean-css')
 const jsdoc = require("gulp-jsdoc3")
 const jshint = require("gulp-jshint")
-//const rimraf = require('rimraf') #SCHDEL
 const { rimraf } = require('rimraf')
 
 const LIBDIR = 'lib'
 const LIBJS = LIBDIR+"/js"
 const LIBCSS = LIBDIR+"/css"
 const LIBIMG = LIBDIR+"/images"
+const LIBFAV = LIBDIR+"/favicons"
 const DOCDIR = './docs'
 
 async function clean() {
   await Promise.all([
     rimraf(LIBJS),
     rimraf(LIBCSS),
-    rimraf(LIBIMG)
+    rimraf(LIBIMG),
+    rimraf(LIBFAV)
     ])
 }
 gulp.task('clean',
@@ -112,25 +112,27 @@ function copy_vendor() {
 function gendoc() {
   var config = require('./jsdoc.json')
   return gulp.src(['README.md', 'src/*.js'], {read: false})
-    .pipe(jsdoc(config));
+    .pipe(jsdoc(config))
 }
 
 function linter() {
   return gulp.src(['src/butil.js', 'src/broad.js', 'src/bgraph.js', 'src/bsandbox.js', 'src/newdesign.js']).pipe(jshint({esversion:8, asi:true, laxbreak:true})).pipe(jshint.reporter('default'))
 }
 
-function images () {
-  // Gulp 5: keep the stream in Buffer-mode so PNGs aren’t re-encoded as UTF-8
+function images () { // Copy images from src to lib
+  // Gulp 5: keep the stream in Buffer-mode so PNGs aren't re-encoded as UTF-8
   return gulp.src('src/images/*', { encoding: false }).pipe(gulp.dest(LIBIMG))
 }
 
-exports.compile = gulp.series(clean,
-  images,
+function favicons () { // Copy favicons from src to lib
+  return gulp.src('src/favicons/*', { encoding: false }).pipe(gulp.dest(LIBFAV))
+}
+
+exports.compile = gulp.series(
+  clean,
+  gulp.parallel(images, favicons),
   compress_js,
-  gulp.parallel(combine_js,
-    combine_jsmin,
-    clean_css),
+  gulp.parallel(combine_js, combine_jsmin, clean_css),
   copy_vendor)
 exports.gendoc = gendoc
-
 exports.jshint = linter
